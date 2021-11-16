48720f2d642c97271db7daa451ed8c84fed71501
Luan Henrique <luan.henriqueepp@gmail.com>
2021-11-16 13:28:03 -0300

    finalizando a implementação da parte de comics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ComicsComponent} from "./comics/comics.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'comics',
+    component: ComicsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
